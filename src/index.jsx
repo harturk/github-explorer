aac8ea17f99efd135bf36afa79ba9402422b39ee
Hártur Kunzler Mainardi <hartur.kunzler@gmail.com>
2021-10-17 17:27:15 -0300

    First Commit

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from 'react-dom';
+import { App } from './App';
+
+// o que se quer renderizar e dentro de qual elemento será renderizado
+render(<App />, document.getElementById('root'))
